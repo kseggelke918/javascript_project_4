b00ecf0f73a487c0b7908a2561e4199a27fd9a5f
kseggelke918 <katie.seggelke@gmail.com>
2020-04-08 13:30:52 -0600

    added rack Cors

--- javascript_project_4_backend/app/controllers/characters_controller.rb
+++ javascript_project_4_backend/app/controllers/characters_controller.rb
@@ -1,2 +1,6 @@
 class CharactersController < ApplicationController
+    def index 
+        characters = Character.all 
+        render json: characters, include: [:name, :status, :location]
+    end 
 end
--- javascript_project_4_backend/app/controllers/houses_controller.rb
+++ javascript_project_4_backend/app/controllers/houses_controller.rb
@@ -1,2 +1,6 @@
 class HousesController < ApplicationController
+    def index 
+        houses = House.all 
+        render json: houses, include: [:name, :motto]
+    end 
 end
