f56ea62781b61b6ad6055c8d654fda72fd74fd10
kseggelke918 <katie.seggelke@gmail.com>
2020-04-09 15:54:40 -0600

    add characters to card

--- javascript_project_4_frontend/src/index.js
+++ javascript_project_4_frontend/src/index.js
@@ -1,31 +1,43 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('The DOM has loaded')
-    getHouses();
-    
+    fetchHouses();
 
 })
+
+function fetchHouses(){
+    fetch("http://localhost:3000/houses")
+        .then (function(response){
+            return response.json();
+        })
+        .then (function(json){
+            json.data.forEach(function(houses){
+                makeCards(houses)
+            })
+        })
+}
 
 function makeCards(houses){
     console.log(houses)
     const div = document.querySelector('.house-container')
     const newDiv = document.createElement('div')
     newDiv.setAttribute("class", "house-card")
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${houses.attributes.name}</h2>`
+    const h3 = document.createElement('h3')
+    h3.innerHTML = `<h3>${houses.attributes.name}</h3>`
     div.appendChild(newDiv)
-    newDiv.appendChild(h2)
-}
+    newDiv.appendChild(h3)
 
-function getHouses(){
-    fetch("http://localhost:3000/houses")
-        .then (function(response){
-            return response.json();
-        })
-        .then (function(json){
-            // console.log(json.data)
-            json.data.forEach(function(houses){
-                makeCards(houses)
-            })
-        })
+    
+
+    const characters = houses.relationships.characters.data
+    characters.forEach(character => {
+        const characterDiv = document.createElement('div')
+        characterDiv.setAttribute("class", "character-card")
+        const p = document.createElement('p')
+        p.innerHTML = `<p>${character.id}</p>`
+        newDiv.appendChild(characterDiv)
+        characterDiv.appendChild(p)
+    })
+
+    // p.innerHTML = `<p>${character.id}</p>`
+    
 }
 
